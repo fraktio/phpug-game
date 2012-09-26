761588fa11e8329a091814042cbaffd615962175
Petri Koivula <pkjedi@iki.fi>
2012-09-26 21:21:02 +0300

    Capistrano deployment

---
+++ config/deploy/production.rb
@@ -0,0 +1,9 @@
+set :node_env, "production"
+set :node_path, "/home/pkjedi/local/bin"
+set :branch, "master"
+set :application_port, "8090"
+set :host, "herpderp.fi"
+set :user, "pkjedi"
+set :admin_runner, "pkjedi"
+set :deploy_to, "/home/pkjedi/www/#{application}/#{node_env}"
+
